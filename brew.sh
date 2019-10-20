b220d4c7157cb14fea9c05033ed15eb325257b8d
Brian Wangila <brian@bwangila.com>
2019-10-20 23:44:14 +0300

    refactor: add descriptions
    – add comments to dots and brew scripts

--- brew.sh
+++ brew.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+ #====================================================================================#
+ # STEP 03: HOMEBREW & MAC APPS..                                                     #
+ # Installing Homebrew along with some common formulae and apps.                      #
+ #====================================================================================#
 
 # Ask for the administrator password upfront.
+# *********************************
 sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
+# Check for Homebrew, and install if we don't have it
+# *********************************
+echo "–– Checking for/installing homebrew.."
 if test ! $(which brew); then
-  echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Make sure we’re using the latest Homebrew.
-echo "Updating homebrew..."
+# *********************************
+echo "–– Updating homebrew.."
 brew update
 
 # Upgrade any already-installed formulae.
-echo "Upgrading homebrew formulae..."
+# *********************************
+echo "–– Upgrading homebrew formulae.."
 brew upgrade --all
 
 # TODO: Add descriptions to everything we are installing
@@ -28,26 +33,33 @@ brew upgrade --all
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-echo "Installing GNU Core utilities..."
+# *********************************
+echo "–– Installing GNU Core utilities.."
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
 # Install some other useful utilities like `sponge`.
+# *********************************
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+# *********************************
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
+# *********************************
 brew install gnu-sed --with-default-names
+
+
 # Install Bash 4.
+# *********************************
+echo "–– Installing Bash 4.."
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
-
 # We installed the new shell, now we have to activate it
-echo "Adding the newly installed shell to the list of allowed shells"
-# Prompts for password
+# *********************************
+echo "–– Adding the newly installed shell to the list of allowed shells--"
+# Prompts for password & change to the new shell
+# *********************************
 sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-# Change to the new shell, prompts for password
 chsh -s /usr/local/bin/bash
 
 # Install `wget` with IRI support.
