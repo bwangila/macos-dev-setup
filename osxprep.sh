083d4e4fe8b4e88e71f2159431969c68b50823ba
Brian Wangila <brian@bwangila.com>
2019-10-20 21:25:29 +0300

    change updating to all available updates

--- osxprep.sh
+++ osxprep.sh
@@ -7,25 +7,15 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Step 1: Update the OS and Install Xcode Tools
-echo "# ***********************************************************************************#"
-echo "# UPDATING OSX.                                                                      #"
-echo "# ***********************************************************************************#"
-
 # Install all available updates
 # *********************************
-echo "# –– Installing all available updates.. In case of a restart, just run the script again."
-echo "# ---------------------------------------"
+echo "–– Installing all available updates.. In case of a restart, run the script again."
+echo "---------------"
 sudo softwareupdate -ia --verbose
-
-# Install only recommended available updates
-# *********************************
-#sudo softwareupdate -ir --verbose
-#echo "–– Installing only recommended updates.. If this requires a restart, just run"
-#echo "   the script again."
 
 # Install Xcode command line tools
 # *********************************
 echo ""
-echo "# –– Installing Xcode Command Line Tools.."
-echo "# ---------------------------------------"
+echo "–– Installing Xcode Command Line Tools.."
+echo "---------------"
 xcode-select --install
